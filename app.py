767d7e6c9bd9386225e60754ce5fcd412af1b2fa
MelvinKim <melobruk1@gmail.com>
2023-06-02 13:20:59 +0300

    chore: add a docstring explaining the request timeout approach

--- app.py
+++ app.py
@@ -8,6 +8,12 @@ app.config['TIMEOUT'] = 30
 
 @app.route('/home')
 def home():
+    """
+    This was to basically display the request timeout functionality.
+    However for production usecases, i would consider the following:
+    1. Implementing distributed tracing to track all the requests served by the servers.
+    2. Implementing logging on the servers to have better visibility on what is happening.
+    """
     time.sleep(40)
     return 'My name is Melvin Kimathi and I love Backend and Site Reliability Engineering.'
 
