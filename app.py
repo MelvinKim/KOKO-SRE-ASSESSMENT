b4a28278cea548bdc7f35c72896bf7ec2570a1fa
MelvinKim <melobruk1@gmail.com>
2023-06-02 13:28:46 +0300

    chore: add status code and mimetype to the response

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from http.client import REQUEST_TIMEOUT
 from urllib import request
-from flask import Flask
+from flask import Flask, json
 import time
 
 app = Flask(__name__)
@@ -15,7 +15,13 @@ def home():
     2. Implementing logging on the servers to have better visibility on what is happening.
     """
     time.sleep(40)
-    return 'My name is Melvin Kimathi and I love Backend and Site Reliability Engineering.'
+    data = "My name is Melvin Kimathi and I love Backend and Site Reliability Engineering."
+    response = app.response_class(
+        response=json.dumps(data),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
 
 if __name__ == '__main__':
     app.run(debug=False, port=5000, threaded=True)
